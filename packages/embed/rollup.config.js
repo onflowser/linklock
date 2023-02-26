071c49f47a6e7e3ba70f7a61c7ffc7674ac8eb4c
Bart <bartolomej.kozorog@gmail.com>
2023-02-26 23:46:17 +0100

    add embed module

---
+++ packages/embed/rollup.config.js
@@ -0,0 +1,16 @@
+import typescript from "@rollup/plugin-typescript";
+import babel from "@rollup/plugin-babel";
+
+export default {
+  input: "src/index.ts",
+  output: {
+    file: "dist/index.js",
+    format: "cjs",
+  },
+  plugins: [
+    typescript(),
+    babel({
+      presets: ["@babel/preset-react"],
+    }),
+  ],
+};
