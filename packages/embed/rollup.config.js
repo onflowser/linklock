8e5db69e7f0847e4983d7e9691e25ac466be2668
Bart <bartolomej.kozorog@gmail.com>
2023-02-27 10:54:12 +0100

    working build

--- packages/embed/rollup.config.js
+++ packages/embed/rollup.config.js
@@ -1,16 +1,30 @@
 import typescript from "@rollup/plugin-typescript";
+import ts from "rollup-plugin-typescript2";
+import json from "@rollup/plugin-json";
 import babel from "@rollup/plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import postcss from "rollup-plugin-postcss";
+// TODO: Do we need this
+// import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import image from "@rollup/plugin-image";
 
 export default {
   input: "src/index.ts",
   output: {
     file: "dist/index.js",
-    format: "cjs",
+    format: "es",
   },
   plugins: [
-    typescript(),
-    babel({
-      presets: ["@babel/preset-react"],
-    }),
+    // peerDepsExternal(),
+    json(),
+    image(),
+    resolve(),
+    commonjs(),
+    ts(),
+    postcss(),
+    // babel({
+    //   presets: ["@babel/preset-react"],
+    // }),
   ],
 };
--- packages/embed/src/index.ts
+++ packages/embed/src/index.ts
@@ -1,18 +1,15 @@
-// import { MembershipCheckout } from "@membership/client";
+import { MembershipCheckout } from "@membership/client";
 import { createRoot } from "react-dom/client";
 
 const rootContainer = document.getElementsByTagName("body")[0];
 
 const reactRoot = createRoot(rootContainer);
 
-// TODO: Add support for using our our React library
-// reactRoot.render(
-// MembershipCheckout({
-//   adminAddress: "",
-//   isOpenModal: true,
-//   membershipDefinitionId: 0,
-//   onCloseModal: () => null,
-// })
-// );
-
-reactRoot.render("Hello World");
+reactRoot.render(
+  MembershipCheckout({
+    adminAddress: "",
+    isOpenModal: true,
+    membershipDefinitionId: 0,
+    onCloseModal: () => null,
+  })
+);
