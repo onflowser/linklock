c7d7ce25c7fed734e50fcbc808f5cf8d28f5c453
Bart <bartolomej.kozorog@gmail.com>
2023-02-27 11:27:38 +0100

    remove import, add comment

--- packages/embed/rollup.config.js
+++ packages/embed/rollup.config.js
@@ -1,13 +1,14 @@
-import typescript from "@rollup/plugin-typescript";
 import ts from "rollup-plugin-typescript2";
 import json from "@rollup/plugin-json";
-import babel from "@rollup/plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import postcss from "rollup-plugin-postcss";
 // TODO: Do we need this
 // import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import image from "@rollup/plugin-image";
+
+// TODO: We need to config polyfills, but that causes other problems.
+//  See: https://github.com/ionic-team/rollup-plugin-node-polyfills/issues/11
 
 export default {
   input: "src/index.ts",
