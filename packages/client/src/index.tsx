f6826f6f0986c65c57754ac1221b2ac0b1f6d308
Bart <bartolomej.kozorog@gmail.com>
2023-02-27 12:25:21 +0100

    Revert "add embed module"
    This reverts commit 071c49f4

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -2,8 +2,6 @@ import * as React from "react";
 export * from "./MembershipCheckout";
 export * from "./providers/root.provider";
 export { useFlow } from "./providers/flow.provider";
-// TODO: Should we move flow.service to separate package,
-//  so that it can be reused on the backend too?
 export * from "./services/flow.service";
 export * from "./hooks/cache";
 export * from "./utils";
