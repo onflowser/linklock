6c0a837f905a3c50192e63301b48caf3cbe27373
jernejgololicic <jernej.gololicic@gmail.com>
2023-02-28 22:03:28 +0100

    - small ui fix: test changes on prod

--- packages/react/src/view/shared/stepper/Stepper.tsx
+++ packages/react/src/view/shared/stepper/Stepper.tsx
@@ -9,7 +9,7 @@ export interface StepperProps {
 export function Stepper({ step, stepTitle }: StepperProps) {
   return (
     <div className="stepper-container">
-      <div className={`steps ${step === 3 && "completed"}`}>
+      <div className={`steps ${step === 3 ? "completed" : ''}`}>
         <Step active={step >= 1}>1</Step>
         <Step active={step >= 2}>2</Step>
         <Step active={step >= 3}>3</Step>
